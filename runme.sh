453ced50cf2bce86b38ab67f4aca65fc1a88ba45
Maciek Sykulski <macieksk@gmail.com>
2018-11-25 03:32:43 +0100

    geo and shiny notebooks update, runme.sh

--- runme.sh
+++ runme.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #ipython notebook
+source ~/.bashrc
 xvfb-run -a jupyter-notebook --no-browser --ip=127.0.0.1 --port=23400 --certfile=/home/hackyeah/mycert.pem 
 #--certfile=~/workspace/certbot/mycert.pem 
